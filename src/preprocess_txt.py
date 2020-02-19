0e423c77121c137142f50f4e89b13616cd41c84e
Andrada Pumnea <andrada.pumnea@futurice.com>
2020-02-19 16:58:26 +0100

    measure processing time

--- src/preprocess_txt.py
+++ src/preprocess_txt.py
@@ -1,11 +1,12 @@
-import concurrent
+import concurrent.futures
 import glob
+import time
 
 from nltk.tokenize import sent_tokenize
 import argparse
 
 
-def format_data_for_bert(input_file):
+def segment_sentences(input_file):
     with open(input_file, 'r') as ro_txt:
         output_file = input_file + '_processed'
         for line in ro_txt:
@@ -18,21 +19,33 @@ def format_data_for_bert(input_file):
     return output_file
 
 
+def run_segmentation_in_parallel():
+    # Create a pool of processes. By default, one is created for each CPU in your machine.
+    #split -l 5000 -d --additional-suffix=.txt $FileName file
+    with concurrent.futures.ProcessPoolExecutor() as executor:
+        # Get a list of files to process
+        input_files = glob.glob(args.input_folder)
+
+        start = time.clock()
+
+        # Process the list of files, but split the work across the process pool to use all CPUs!
+        for input_file, output_file in zip(input_files, executor.map(segment_sentences, input_files)):
+            print(f"A preprocessing for {input_file} was saved as {output_file}")
+
+        elapsed = time.clock()
+        print("Time spent in current batch is: ", elapsed - start)
+
+
 def parse_args():
     parser = argparse.ArgumentParser()
-    parser.add_argument('--input_file', help='Provide text file to be processed')
+    parser.add_argument('--input_folder', help='Provide the folder where the text data needs to be processed')
     args = parser.parse_args()
     return args
 
 
 if __name__ == '__main__':
     args = parse_args()
-    # Create a pool of processes. By default, one is created for each CPU in your machine.
-    with concurrent.futures.ProcessPoolExecutor() as executor:
-        # Get a list of files to process
-        input_files = glob.glob("*.txt")
-
-        # Process the list of files, but split the work across the process pool to use all CPUs!
-        for input_file, output_file in zip(input_files, executor.map(format_data_for_bert, input_files)):
-            print(f"A preprocessing for {input_file} was saved as {output_file}")
-    # format_data_for_bert(args.input_file, args.output_file)
+    start = time.clock()
+    run_segmentation_in_parallel()
+    elapsed = time.clock()
+    print("Time spent in run_segmentation_in_parallel is: ", elapsed - start)
