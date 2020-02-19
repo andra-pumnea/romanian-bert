0c75360ebe51321e67ae9989f968d5a95ba967f3
Andrada Pumnea <andrada.pumnea@futurice.com>
2020-02-19 12:39:02 +0100

    add argument for output file

--- src/preprocess_txt.py
+++ src/preprocess_txt.py
@@ -2,12 +2,12 @@ from nltk.tokenize import sent_tokenize
 import argparse
 
 
-def format_data_for_bert(ro_data):
-    with open(ro_data, 'r') as ro_txt:
+def format_data_for_bert(input_file, output_file):
+    with open(input_file, 'r') as ro_txt:
         for line in ro_txt:
             doc = sent_tokenize(line)
             for sent in doc:
-                with open('ro_data/ro_dedup_processed.txt', 'a') as ro_processed:
+                with open(output_file, 'a') as ro_processed:
                     ro_processed.write("%s\n" % sent)
                 ro_processed.close()
     ro_txt.close()
@@ -16,10 +16,11 @@ def format_data_for_bert(ro_data):
 def parse_args():
     parser = argparse.ArgumentParser()
     parser.add_argument('--input_file', help='Provide text file to be processed')
+    parser.add_argument('--output_file', help='Provide text file where the output will be found')
     args = parser.parse_args()
     return args
 
 
 if __name__ == '__main__':
     args = parse_args()
-    format_data_for_bert(args.input_file)
+    format_data_for_bert(args.input_file, args.output_file)
